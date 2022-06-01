2a7c5b05135303b55e6261bbd648bd6f3e85f8ad
vishalgwl <33472194+vishalgwl@users.noreply.github.com>
2022-06-02 07:57:27 +0900

    Update prawas.cpp

--- prawas.cpp
+++ prawas.cpp
@@ -35,6 +35,7 @@ static int week_end2 = 5;
 
 #define SIZE 14
 #define Allowed_PRAWAS 2 //weekly
+#define NO_OF_SHAKHA 8
 
 int visited[SIZE] = { 0 };
 int week_visitied[SIZE] = { 0 };
@@ -42,12 +43,18 @@ int week_visitied[SIZE] = { 0 };
 bool bdone = false;
 int day = 0;
 bool found = false;
+int remaing_for_prawas = SIZE;
+int shakha_limit = NO_OF_SHAKHA;
 
 bool isVisitAllowed(int ii, int prawas_karyakarta_index[8])
 {
 	bool bret = true;
-	for (int i = ii; i >= 0;i--)
+	if (ii < 0)
 	{
+		return bret;
+	}
+	for (int i = ii; i >= 0;i--)
+	{//issue
 		if (week_visitied[prawas_karyakarta_index[i]] >= Allowed_PRAWAS)
 		{
 			bret = false;
@@ -59,13 +66,18 @@ bool isVisitAllowed(int ii, int prawas_karyakarta_index[8])
 
 void check(int count, int index, char prawas[][256],int prawas_karyakarta_index[8])
 {
-	if (count == 8)
+	//if (count == NO_OF_SHAKHA)
+	
+	if( count == shakha_limit)
 	{
-		for (int i = 0; i < count; i++)
+		for (int i = 0; i < shakha_limit; i++)
 			cout << prawas[i] << " ";
 		cout << endl;
 		day++;
 		found = true;
+
+		if (remaing_for_prawas < shakha_limit) 
+			shakha_limit--;
 		
 		if (day == 5)
 		{
@@ -79,16 +91,19 @@ void check(int count, int index, char prawas[][256],int prawas_karyakarta_index[
 			break;
 		}
 		
-		if (!visited[i] && isVisitAllowed(count, prawas_karyakarta_index) && week_visitied[i] < Allowed_PRAWAS)
+		if (!visited[i] && isVisitAllowed(count -1, prawas_karyakarta_index) && week_visitied[i] < Allowed_PRAWAS)
 		{
 			visited[i]++;
 			week_visitied[i]++;
+			remaing_for_prawas = (week_visitied[i] == Allowed_PRAWAS) ? (remaing_for_prawas - 1) : remaing_for_prawas;
+			
 			if (found)
 			{
 				found = false;
 				for (int ii = count-1; ii >= 0;ii--)
 				{
 					week_visitied[prawas_karyakarta_index[ii]]++;
+					remaing_for_prawas = (week_visitied[prawas_karyakarta_index[ii]] == Allowed_PRAWAS) ? (remaing_for_prawas - 1) : remaing_for_prawas;
 				}
 
 			}
@@ -98,6 +113,7 @@ void check(int count, int index, char prawas[][256],int prawas_karyakarta_index[
 			prawas_karyakarta_index[count] = i;
 			check(count + 1, i, prawas, prawas_karyakarta_index);
 			visited[i]--;
+
 			strcpy(prawas[count],"");
 			prawas_karyakarta_index[count] = 0;
 			
